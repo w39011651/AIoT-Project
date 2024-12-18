131955f58841ba9d618e3604d98c45b918dd8535
Giant <alter39011651@gmail.com>
2024-12-18 22:32:17 +0800

    bytecoding

--- getFrame.py
+++ getFrame.py
@@ -1,4 +1,5 @@
 import requests
+from flask import Response
 import numpy as np
 import cv2
 
@@ -8,6 +9,7 @@ url = 'http://127.0.0.1:5000'
 
 while True:
     img_res = requests.get(url)
+    print(Response.status_code)
     img_arr = np.frombuffer(img_res.content, dtype = np.uint8)
     img = cv2.imdecode(img_arr, cv2.IMREAD_COLOR)
     cv2.imshow('ResponseImage', img)
